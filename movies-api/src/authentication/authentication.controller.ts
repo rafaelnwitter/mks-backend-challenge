33c3da072d2f1b5cc3d74d8630d3038fb92f34e8
Rafael Witt <rafawitt@hotmail.com>
2022-09-01 18:32:47 -0300

    chore: new project with auth module and movies initial module

---
+++ movies-api/src/authentication/authentication.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Req, Controller, HttpCode, Post, UseGuards, Response, Res, HttpStatus, Get } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import RegisterDto from './dto/register.dto';
+import RequestWithUser from './requestWithUser.interface';
+import { LocalAuthenticationGuard } from './localAuthentication.guard';
+import JwtAuthenticationGuard from './jwt-authentication.guard';
+
+
+@Controller('authentication')
+export class AuthenticationController {
+  constructor(
+    private readonly authenticationService: AuthenticationService
+  ) { }
+
+  @Post('register')
+  async register(@Body() registrationData: RegisterDto) {
+    return this.authenticationService.register(registrationData);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthenticationGuard)
+  @Post('log-in')
+  async logIn(@Req() request: RequestWithUser, @Res() response: Response) {
+    const { user } = request;
+    const cookie = this.authenticationService.getCookieWithJwtToken(user.id);
+    console.log(cookie)
+    response.headers.set('Set-Cookie', cookie);
+    user.password = undefined;
+    return response.status;
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get()
+  authenticate(@Req() request: RequestWithUser) {
+    const user = request.user;
+    user.password = undefined;
+    return user;
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post('log-out')
+  async logOut(@Req() request: RequestWithUser, @Res() response: Response) {
+    response.headers.set('Set-Cookie', this.authenticationService.getCookieForLogOut());
+    return response.status;
+  }
+}
---
+++ movies-api/src/movie/movie.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+
+@Controller('movie')
+export class MovieController {
+  constructor(private readonly movieService: MovieService) {}
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  create(@Body() createMovieDto: CreateMovieDto) {
+    return this.movieService.createMovie(createMovieDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.movieService.getAllMovies();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.movieService.getMovieById(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.movieService.update(+id, updateMovieDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.movieService.remove(+id);
+  }
+}
