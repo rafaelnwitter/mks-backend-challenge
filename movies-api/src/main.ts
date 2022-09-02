b8f5b0f6fd6fa0fd19130c5330d7f5d1aa330797
Rafael Witt <rafawitt@hotmail.com>
2022-09-02 10:41:06 -0300

    feat: swagger and compodoc dependencies

--- movies-api/src/main.ts
+++ movies-api/src/main.ts
@@ -1,13 +1,28 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API with NestJS to MKS Backend Test Development')
+    .setDescription('API developed throughout the API with NestJS course')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const port = configService.get('PORT') ?? 3000;
+
+  await app.listen(port);
 }
 bootstrap();
