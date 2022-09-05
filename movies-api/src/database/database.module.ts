ba569abf5074a2a2f825edf34334a76bb20a4ac5
Rafael Witt <rafawitt@hotmail.com>
2022-09-04 21:09:56 -0300

    fix: not used import

--- movies-api/src/database/database.module.ts
+++ movies-api/src/database/database.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-// import DatabaseLogger from './databaseLogger';
 
 @Module({
   imports: [
@@ -10,7 +9,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-     //   logger: new DatabaseLogger(),
         host: configService.get('POSTGRES_HOST'),
         port: configService.get('POSTGRES_PORT'),
         username: configService.get('POSTGRES_USER'),
